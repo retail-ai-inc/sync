8eb664dbba8683357c127cb8b97b80a0c269f9d1
Jack(ZhangYongguang) <zhang_yongguang@c.tre-inc.com>
2025-03-21 13:11:37 +0800

    Merge main into feature/ui

--- pkg/syncer/test/common_test.go
+++ pkg/syncer/test/common_test.go
@@ -230,11 +230,11 @@ func compareMongoDoc(srcDoc, tgtDoc bson.M, srcCollection string, mappings []con
 
 func buildMongoDSN(conn ConnDetail) string {
 	if conn.User != "" && conn.Password != "" {
-		return fmt.Sprintf("mongodb://%s:%s@%s:%s/%s?directConnection=true&authSource=admin", 
+		return fmt.Sprintf("mongodb://%s:%s@%s:%s/%s?directConnection=true&authSource=admin",
 			conn.User, conn.Password, conn.Host, conn.Port, conn.Database)
 	}
-	
-	return fmt.Sprintf("mongodb://%s:%s/%s?directConnection=true", 
+
+	return fmt.Sprintf("mongodb://%s:%s/%s?directConnection=true",
 		conn.Host, conn.Port, conn.Database)
 }
 func buildMySQLDSN(conn ConnDetail) string {
